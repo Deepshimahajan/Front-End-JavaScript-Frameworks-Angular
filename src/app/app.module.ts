0de4c24e5945dc69e464b5f0958b21cb9eb25faf
Deepshi Mahajan <dmahajan@scu.edu>
2018-07-31 13:38:38 -0700

    Template Forms Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule } from '@angular/material/toolbar'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { AppComponent } from './app.component';
+
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
-import { MatListModule } from '@angular/material/list';
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
-import { PromotionService } from './services/promotion.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PromotionService } from './services/promotion.service';
+import { LoginComponent } from './login/login.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 
 @NgModule({
   declarations: [
@@ -27,22 +37,32 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent,
     AboutComponent,
-    ContactComponent
+    HomeComponent,
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
-    MatListModule,
     MatToolbarModule,
     FlexLayoutModule,
+    MatListModule,
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
-    AppRoutingModule
+    FormsModule,
+    MatDialogModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService,PromotionService],
+  entryComponents: [
+    LoginComponent
+],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
---
+++ src/app/services/leader.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { LEADERS } from '../shared/leaders';
+import { Leader } from '../shared/leader';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LeaderService {
+
+  LEADERS: Leader[];
+
+  constructor() { }
+
+
+  getLeaders(): Leader[] {
+    return LEADERS;
+  }
+
+  getFeaturedLeader(): Leader {
+    return LEADERS.filter((leader) => leader.featured)[0];
+  }
+
+}
